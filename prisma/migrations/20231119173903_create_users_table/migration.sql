c1638160f3fdf99129d07d0380d2063e00c7c928
Mateus <mateusmba20@gmail.com>
2023-11-19 14:42:11 -0300

    delete all migrations, drop fincheck database and start again

--- prisma/migrations/20231119173903_create_users_table/migration.sql
+++ prisma/migrations/20231119173903_create_users_table/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "users" (
     "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
